dd3ac42bb05268f0f7f3116e6f41080a275dd29d
Devin Beliveau <devinmarieb@gmail.com>
2019-01-15 23:58:28 +0100

    Updates

---
+++ precache-manifest.7d1c35b03438bb26dcbd9cd1253a8cd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f619f12a6c6df005dc",
+    "url": "/my-app/static/css/main.5a21b65d.chunk.css"
+  },
+  {
+    "revision": "72f619f12a6c6df005dc",
+    "url": "/my-app/static/js/main.72f619f1.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/my-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "c0c97bfd991710c7d7d8cfb9a15cf27b",
+    "url": "/my-app/index.html"
+  }
+];
